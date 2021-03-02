0d385d784eca5121758573a7e45fdf33dacd8a44
Ryan Mendez <rjmendez80@gmail.com>
2021-03-02 11:28:24 -0800

    add pages folder with main and portfolio

---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import AboutMe from '../components/AboutMe';
+
+class Main extends React.Component {
+  render() {
+    return <AboutMe />;
+  }
+}
+
+export default Main;
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import PortfolioCards from '../components/PortfolioCards';
+
+class Portfolio extends React.Component {
+  render() {
+    return <PortfolioCards />;
+  }
+}
+
+export default Portfolio;
